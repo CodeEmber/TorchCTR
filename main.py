383795ccfd15eeb9bffa453a6e1fe93b9df51b04
wyx-hhhh <1360479992@qq.com>
2024-03-27 18:22:00 +0800

    ✨ feat(main文件): 添加运行时间的输出
    添加开始时间和总的耗时

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 '''
 Author       : wyx-hhhh
 Date         : 2023-10-28
-LastEditTime : 2024-03-22
+LastEditTime : 2024-03-27
 Description  : 
 '''
 import subprocess
+import time
 from managers.logger_manager import logger
 import traceback
 import os
@@ -14,10 +15,16 @@ os.environ["MKL_THREADING_LAYER"] = "GNU"
 models = ["din"]
 for model in models:
     try:
-        logger.info(f"开始运行{model}模型")
-        logger.send_message(f"开始运行{model}模型", message_type=2)
+        start_time = time.time()
+        logger.info(f"开始运行{model}模型，开始时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}")
+        logger.send_message(f"开始运行{model}模型，开始时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}", message_type=2)
         subprocess.run(f"python -m models.{model}.run_expid", shell=True)
-        logger.send_message(f"运行{model}模型成功", message_type=2, mention=True)
+        end_time = time.time()
+        run_time = end_time - start_time
+        hours, rem = divmod(run_time, 3600)
+        minutes, seconds = divmod(rem, 60)
+        logger.info(f"运行{model}模型成功，耗时{int(hours):0>2}:{int(minutes):0>2}:{seconds:05.2f}")
+        logger.send_message("运行{}模型成功，耗时{:0>2}:{:0>2}:{:05.2f}".format(model, int(hours), int(minutes), seconds), message_type=2, mention=True)
     except subprocess.CalledProcessError as e:
         logger.error(f"运行{model}失败，错误信息如下：{e}")
         continue
