3740e587ceeacb24d921cafee4e362c0ea207bbd
wyx-hhhh <1360479992@qq.com>
2024-05-27 12:47:46 +0800

    🦄 refactor(din模型): 添加评估管理器
    添加评估管理器；修改类名称

--- models/din/test_expid.py
+++ models/din/test_expid.py
@@ -1,7 +1,7 @@
 '''
 Author       : wyx-hhhh
 Date         : 2024-03-08
-LastEditTime : 2024-03-25
+LastEditTime : 2024-05-23
 Description  : 
 '''
 import os
@@ -10,33 +10,33 @@ import torch
 
 from models.din.train_config import train_config
 from models.din.model import DIN
-from managers import ConfigManager, DataManager, LoggerManager, TrainManager, SaveManager
+from managers import ConfigManager, DataManager, LoggerManager, TrainManager, SaveManager, EvaluationManager
 from utils.evaluation import gauc
 from utils.file_utils import get_new_file_path
 from utils.torch_utils import set_device
 from managers.logger_manager import logger
 from utils.utilities import get_values_by_keys
 
-ConfigManager = ConfigManager(train_config=train_config)
-config = ConfigManager.get_config()
-logger = LoggerManager(config=config)
-DataManager = DataManager(config=config)
-data_dict = DataManager.data_process()
-test_df, test_dataloader, enc_dict = get_values_by_keys(data_dict, ['test_df', 'test_dataloader', 'enc_dict'])
-TrainManager = TrainManager(config=config).trainer
+config_manager = ConfigManager(train_config=train_config)
+config = config_manager.get_config()
+# logger = LoggerManager(config=config)
+# data_manager = DataManager(config=config)
+# data_dict = data_manager.data_process()
+# test_df, test_dataloader, enc_dict = get_values_by_keys(data_dict, ['test_df', 'test_dataloader', 'enc_dict'])
+evaluation_manager = EvaluationManager(config=config, logger=logger)
+train_manager = TrainManager(config=config).trainer
 
-model = DIN(enc_dict=enc_dict)
-device = set_device(config['device'])
-model_file_path = get_new_file_path(['results', config["model_name"], 'save_model'])
-if not os.path.exists(model_file_path):
-    raise FileNotFoundError(f"模型文件{model_file_path}不存在")
-model.load_state_dict(torch.load(model_file_path))
-model = model.to(device)
+# model = DIN(enc_dict=enc_dict)
+# device = set_device(config['device'])
+# model_file_path = get_new_file_path(['results', config["model_name"], 'save_model'])
+# if not os.path.exists(model_file_path):
+#     raise FileNotFoundError(f"模型文件{model_file_path}不存在")
+# model.load_state_dict(torch.load(model_file_path))
+# model = model.to(device)
 
-test_predictions = TrainManager.get_test_predict(model, test_dataloader, device)
-test_df['prediction'] = test_predictions
-test_df['ranking'] = test_df.groupby('user_id')['prediction'].rank(ascending=False, method='first')
+# test_predictions = train_manager.get_test_predict(model, test_dataloader, device)
+# test_df['prediction'] = test_predictions
+# test_df['ranking'] = test_df.groupby('user_id')['prediction'].rank(ascending=False, method='first')
 
-logger.info("开始测试模型")
-gauc = gauc(test_df, 'user_id', 'label', 'prediction')
-logger.info(f"测试集GAUC: {gauc}")
+# gauc_result = evaluation_manager.get_eval_res(test_df=test_df, mode='eval')
+# logger.info(f"测试集GAUC: {gauc_result}")
--- models/din/train_config.py
+++ models/din/train_config.py
@@ -1,7 +1,7 @@
 '''
 Author       : wyx-hhhh
 Date         : 2023-10-28
-LastEditTime : 2024-04-11
+LastEditTime : 2024-05-23
 Description  : 
 '''
 #参数配置
@@ -9,9 +9,34 @@ train_config = {
     "model_name": "din",
     "data": "amazon",
     "trainer": "dl",
-    "debug_mode": False,
+    "debug_mode": True,
     "epoch": 10,
     "batch_size": 1024,
     "lr": 0.0001,
-    "device": 0,
+    "device": -1,
+    "metric_func": {
+        "train": [
+            {
+                "eval_func": "auc"
+            },
+            {
+                "eval_func": "log_loss"
+            },
+        ],
+        "eval": [
+            {
+                "eval_func": "ndcg",
+                "k": [10]
+            },
+            {
+                "eval_func": "gauc"
+            },
+        ],
+    },
+    "col_name": {
+        "user_col": "user_id",
+        "ranking_col": "ranking",
+        "label_col": "label",
+        "pre_col": "prediction",
+    },
 }
