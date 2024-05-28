13021e1759e6abdf38dce3552c14fd55f82e3ccf
wyx-hhhh <1360479992@qq.com>
2024-05-28 19:52:11 +0800

    ✨ feat(DIN模型): 添加DIN模型的训练部分

---
+++ models/ddin/train_config.py
@@ -0,0 +1,17 @@
+'''
+Author       : wyx-hhhh
+Date         : 2024-04-24
+LastEditTime : 2024-05-07
+Description  : 
+'''
+#参数配置
+train_config = {
+    "model_name": "ddin",
+    "data": "pixelrec",
+    "trainer": "dl",
+    "debug_mode": True,
+    "epoch": 10,
+    "batch_size": 1024,
+    "lr": 0.0001,
+    "device": -1,
+}
